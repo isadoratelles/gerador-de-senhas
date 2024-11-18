f0f738003b108a85543f270c04636f8d99e69893
Isadora Telles <128058321+isadoratelles@users.noreply.github.com>
2024-11-18 11:36:41 -0300

    Add files via upload

--- site de seg/main.js
+++ site de seg/main.js
@@ -1,5 +1,18 @@
 const numeroSenha=document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
-const botoes=document.querySelector('.parametro-senha-botoes');
+const botoes=document.querySelectorAll('.parametro-senha-botao');
 botoes[0].onclick=diminuirTamanho;
+botoes[1].onclick=aumentarTamanho;
+function diminuirTamanho(){
+    if(tamanhoSenha>6){
+        tamanhoSenha=tamanhoSenha-1;
+        numeroSenha.textContent=tamanhoSenha; 
+    }
+}
+function aumentarTamanho(){
+    if(tamanhoSenha<12){
+        tamanhoSenha=tamanhoSenha+1;
+        numeroSenha.textContent=tamanhoSenha; 
+    }
+}
